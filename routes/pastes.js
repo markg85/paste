855df051981b0e8eaccb72c063d19032758d19bf
Mark Gaiser <markg85@gmail.com>
2017-07-29 22:26:13 +0200

    Initial server side changes. var replaces with let as well. It's not using any templates anymore. Every response is a JSON response.

--- routes/pastes.js
+++ routes/pastes.js
@@ -1,16 +1,16 @@
-var shortid = require('shortid');
-var mongoose = require('mongoose');
-var crypto = require('crypto');
-var fs = require('fs');
-var path = require('path');
-var appDir = path.dirname(require.main.filename);
-var algorithm = 'aes-256-ctr';
+let shortid = require('shortid');
+let mongoose = require('mongoose');
+let crypto = require('crypto');
+let fs = require('fs');
+let path = require('path');
+let appDir = path.dirname(require.main.filename);
+let algorithm = 'aes-256-ctr';
 
-var Schema = mongoose.Schema
+let Schema = mongoose.Schema
 
-var lifeTimes = [3600, 14400, 86400, 604800, 1209600, 2419200, 31536000]
+let lifeTimes = [3600, 14400, 86400, 604800, 1209600, 2419200, 31536000]
     
-var pasteSchema = new Schema({
+let pasteSchema = new Schema({
     _id: { type: String, unique: true, 'default': shortid.generate },
     date: { type: Date, default: Date.now },
     language: String,
@@ -19,82 +19,80 @@ var pasteSchema = new Schema({
     data: String
 });
 
-var Paste = mongoose.model('Pastes', pasteSchema);
+let Paste = mongoose.model('Pastes', pasteSchema);
 
 function encrypt(text){
-    var current_date = (new Date()).valueOf().toString();
-    var random = Math.random().toString();
-    var password = crypto.createHash('sha1').update(current_date + random).digest('hex');
-    var cipher = crypto.createCipher(algorithm, password)
-    var crypted = cipher.update(text,'utf8','hex')
+    let current_date = (new Date()).valueOf().toString();
+    let random = Math.random().toString();
+    let password = crypto.createHash('sha1').update(current_date + random).digest('hex');
+    let cipher = crypto.createCipher(algorithm, password)
+    let crypted = cipher.update(text,'utf8','hex')
     crypted += cipher.final('hex');
     
     return {password: password, data: crypted};
 }
  
 function decrypt(text, password){
-  var decipher = crypto.createDecipher(algorithm, password)
-  var dec = decipher.update(text,'hex','utf8')
+  let decipher = crypto.createDecipher(algorithm, password)
+  let dec = decipher.update(text,'hex','utf8')
   dec += decipher.final('utf8');
   return dec;
 }
 
 exports.paste = function(req, res){
-    var id = req.params.id
-    var responseText = "";
+    let id = req.params.id
+    let responseText = "";
   
     if(!id) {
         responseText = "No paste id provided."
     } else {
         mongoose.model("Pastes").findOne({_id: id}, function(err, n){
             if (n) {
-                res.render('paste', {title: "Paste", url: n._id, data: n});
+                res.json(n);
             } else {
-                res.status(404).send("Paste not found.\n");
+                res.status(404).json({ error: "Paste not found." });
             }
         });
     }
 };
 
 exports.pasteDecrypt = function(req, res){
-    var id = req.params.id
-    var responseText = "";
+    let id = req.params.id
+    let responseText = "";
     
     if(!id) {
         responseText = "No paste id provided."
     } else {
         mongoose.model("Pastes").findOne({_id: id}, function(err, n){
             if (n) {
-              n.data = decrypt(n.data, req.params.decryptKey)
-              res.render('paste', {title: "Paste", url: n._id+"/"+req.params.decryptKey, data: n});
+              res.json(n);
             } else {
-              res.status(404).send("Paste not found.\n");
+              res.status(404).json({ error: "Paste not found." });
             }
         });
     }
 };
 
 exports.raw = function(req, res){
-    var id = req.params.id
-    var responseText = "";
+    let id = req.params.id
+    let responseText = "";
   
     if(id) {
         mongoose.model("Pastes").findOne({_id: id}, function(err, n){
           if (n) {
-            res.type('text/plain');
-            res.send(n.data);
+            res.json(n);
           } else {
-            res.status(404).send("Paste not found.\n");
+            res.status(404).json({ error: "Paste not found." });
           }
         });
     } else {
-      res.status(404).send("Paste not found.\n");
+      res.status(404).json({ error: "Paste not found." });
     }
 };
 
 exports.rawDecrypt = function(req, res){
-    var id = req.params.id
-    var responseText = "";
+    let id = req.params.id
+    let responseText = "";
   
     if(id) {
         mongoose.model("Pastes").findOne({_id: id}, function(err, n){
@@ -103,22 +101,10 @@ exports.rawDecrypt = function(req, res){
             res.type('text/plain');
             res.send(n.data);
           } else {
-            res.status(404).send("Paste not found.\n");
+            res.status(404).json({ error: "Paste not found." });
           }
         });
     }
-};
-
-exports.pastes = function(req, res){
-    /*
-    mongoose.model("Pastes").find(function(err, pastes){
-        console.log(err);
-        console.log(pastes)
-        res.render('index', {title: "Paste service", pasteList: pastes});
-    });
-    */
-    
-    res.render('index', {title: "Paste service"});
 };
 
 exports.create = function(req, res){
@@ -134,55 +120,51 @@ exports.create = function(req, res){
     // Weird.. the "encryptPaste" value is a string, not a bool.. It does contain only "true" or "false" so just parsing it fixes it.
     req.body.encryptPaste = JSON.parse(req.body.encryptPaste)
 
-    var pasteData = req.body.data;
-    var decryptKey = "";
+    let pasteData = req.body.data;
+    let decryptKey = "";
     
     if (req.body.encryptPaste) {
-        var encryptResult = encrypt(pasteData);
+        let encryptResult = encrypt(pasteData);
         pasteData = encryptResult.data;
         decryptKey = encryptResult.password;
     }
     
     if (req.body.data == "") {
-        res.send({ hash: "", error:"Empty paste received. This is not allowed!" })
+        res.status(500).send({ error: "Empty paste received. This is not allowed!" })
         return;
     }
     
-    var newPaste;
+    let newPaste;
     if (req.body.language) {
-        newPaste = new Paste({ language: req.body.language, data: pasteData, lifetime: req.body.lifetime, encrypted: req.body.encryptPaste });
-        newPaste.save(function(err){
-                    
-                    if(!err) {
-                        //console.log("OK...")
-                    } else {
-                        //console.log(err)
-                    }
-                    
-                });
+      newPaste = new Paste({ language: req.body.language, data: pasteData, lifetime: req.body.lifetime, encrypted: req.body.encryptPaste });
+
+      newPaste.save(function(err){
+        if(err) {
+          res.error(500).json({ error: err });
+        }
+      });
     }
-    res.send({ hash: newPaste._id, decryptKey: decryptKey, error:"none" })
+
+    res.json({ hash: newPaste._id, decryptKey: decryptKey })
 };
 
 exports.uploadData = function(req, res) {
-  var fullUrl = req.protocol + '://' + req.get('host') + '/data/' + req.files[0].filename + '\n';
-  res.writeHead(200, {"context-type":"text/plain"});
-  res.write(fullUrl);
-  res.end();
+  let fullUrl = req.protocol + '://' + req.get('host') + '/data/' + req.files[0].filename + '\n';
+  res.json({ url: fullUrl });
 }
 
 exports.getData = function(req, res) {
-  var file = req.params.file;
+  let file = req.params.file;
 
   if (file == "") {
-    res.status(404).send("No valid input given.\n")
+    res.status(404).json({ error: "No valid input given.\n" })
     return;
   }
 
   try {
     fs.accessSync(appDir + '/uploads/' + file);
   } catch (e) {
-    res.status(404).send("File not found on server. Its probably deleted or never uploaded.\n");
+    res.status(404).json({ error: "File not found on server. Its probably deleted or never uploaded.\n"});
     return;
   }
 
@@ -190,15 +172,15 @@ exports.getData = function(req, res) {
 }
 
 exports.createRest = function(req, res){
-    var language = req.params.language;
-    var lifetime = 0; // unlimited
+    let language = req.params.language;
+    let lifetime = 0; // unlimited
 
     if (req.body.data == "") {
-        res.send({ hash: "", error:"Empty paste received. This is not allowed!" })
+        res.json({ hash: "", error:"Empty paste received. This is not allowed!" })
         return;
     }
     
-    var newPaste = new Paste({ language: language, data: req.body, lifetime: lifetime, encrypted: false });
+    let newPaste = new Paste({ language: language, data: req.body, lifetime: lifetime, encrypted: false });
     
     newPaste.save(function(err){
       if(!err) {
@@ -208,8 +190,10 @@ exports.createRest = function(req, res){
       }
     });
 
-    var fullUrl = req.protocol + '://' + req.get('host') + '/' + newPaste._id + '\n';
-    res.writeHead(200, {"context-type":"text/plain"});
-    res.write(fullUrl);
-    res.end();
+    let fullUrl = req.protocol + '://' + req.get('host') + '/' + newPaste._id + '\n';
+    res.json({ id: newPaste._id, url: fullUrl });
+};
+
+exports.fallback = function(req, res){
+  res.error(404).json({ error: "This route does not exist!" });
 };
--- server.js
+++ server.js
@@ -1,13 +1,12 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var server = require('http').Server(app);
-var mongoose = require('mongoose');
-var path = require('path');
-var swig = require('swig');
-var multer = require('multer');
+let express = require('express');
+let app = express();
+let bodyParser = require('body-parser');
+let server = require('http').Server(app);
+let mongoose = require('mongoose');
+let path = require('path');
+let multer = require('multer');
 
-var storage = multer.diskStorage({
+let storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, __dirname + '/uploads')
   },
@@ -16,13 +15,13 @@ var storage = multer.diskStorage({
   }
 })
 
-var upload = multer({storage: storage,
+let upload = multer({storage: storage,
                      limits: {fileSize: 10485760}}); // 10 MiB limit
 
 server.listen(80);
 
 // Database connection
-var db = mongoose.connection;
+let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function callback () {
   console.log("database connection is open!")
@@ -34,9 +33,7 @@ mongoose.connect('mongodb://mongo/paste', {
   useMongoClient: true
 });
 
-swig.setDefaults({ cache: false });
-
-var pasteRoutes = require('./routes/pastes');
+let pasteRoutes = require('./routes/pastes');
 exports.app = app
 
 app.use(bodyParser.json());
@@ -44,14 +41,6 @@ app.use(bodyParser.text());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.engine('html', swig.renderFile);
-app.set('view engine', 'html');
-app.set('views', __dirname + '/views');
-
-// Set routes
-app.get('/', pasteRoutes.pastes);
 
 // Upload for data
 app.get('/data/:file', pasteRoutes.getData);
@@ -67,3 +56,7 @@ app.post('/data', upload.any(), pasteRoutes.uploadData);
 // Create a paste where the language is in the url. All the data is.. well.. in the data.
 // This does prevent provinding options like lifetime, but on the other hand opens the door to easily paste from the command line using curl.
 app.post('/:language', upload.any(), pasteRoutes.createRest);
+
+// Default fallback routes.
+app.get('*', pasteRoutes.fallback);
+app.post('*', pasteRoutes.fallback);
